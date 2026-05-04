9a9d8416fa8f139adb2037e540696cb3a3381fb7
Yashodara De Silva <yashodaradesilva166@gmail.com>
2026-05-04 16:25:48 +0530

    .

--- smartlogix-mobile/src/screens/admin/modules/DriverMonitorModuleScreen.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import DriverMonitorNavigator from './driver-monitor/DriverMonitorNavigator';
-
-export default function DriverMonitorModuleScreen() {
-    return <DriverMonitorNavigator />;
-}
