b0beb5385d560c3ed8ba6337b0ca1b87634bf224
Yashodara De Silva <yashodaradesilva166@gmail.com>
2026-05-04 16:26:18 +0530

    .

--- smartlogix-mobile/src/screens/admin/modules/TripRefusalsModuleScreen.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import TripRefusalsNavigator from './trip-refusals/TripRefusalsNavigator';
-
-export default function TripRefusalsModuleScreen() {
-    return <TripRefusalsNavigator />;
-}
-
