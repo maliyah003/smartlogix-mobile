5f6640b9262a33171797706b6790be4f322048fc
Yashodara De Silva <yashodaradesilva166@gmail.com>
2026-05-04 16:25:28 +0530

    .

--- smartlogix-mobile/src/screens/admin/modules/DriverIncidentsModuleScreen.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import DriverIncidentsScreen from './driver-monitor/DriverIncidentsScreen';
-
-export default function DriverIncidentsModuleScreen() {
-    return <DriverIncidentsScreen />;
-}
