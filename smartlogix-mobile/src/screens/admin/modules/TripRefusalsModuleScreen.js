b6468844ffc2ca427aff6f0fc93e0dc1027fae8a
Yashodara De Silva <yashodaradesilva166@gmail.com>
2026-05-04 16:27:24 +0530

    Add files via upload

---
+++ smartlogix-mobile/src/screens/admin/modules/TripRefusalsModuleScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import TripRefusalsNavigator from './trip-refusals/TripRefusalsNavigator';
+
+export default function TripRefusalsModuleScreen() {
+    return <TripRefusalsNavigator />;
+}
+
